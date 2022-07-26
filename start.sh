064e376464704c37002142e1b9dadae678316940
thomaspara <thomasjohnparashos@gmail.com>
2022-07-25 22:41:04 -0700

    bash pls

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 while [ -z "$(fping google.com | grep alive)" ]
 do
     echo "waiting for internet ..."
@@ -6,6 +7,6 @@ done
 echo "Internet is now online"
 script_full_path=$(dirname "$(realpath "$0")")
 cd $script_full_path
-source ./venv/bin/activate
+source venv/bin/activate
 python3 impostor_bot.py
 sleep 3000000
